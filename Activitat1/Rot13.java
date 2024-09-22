141f8639475e86eb47378065cccac0f59e7606b5
Maria <mmanzanoroldan@gmail.com>
2024-09-22 11:12:21 +0200

    Fet el xifrat

--- Activitat1/Rot13.java
+++ Activitat1/Rot13.java
@@ -1,23 +1,29 @@
 public class Rot13 {
-    public static char[] maj = {'A','B','C','Ç','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    public static char[] min = {'a','b','c','ç','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
-    
+    public static char[] maj = {'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    public static char[] min = {'a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
     public static String xifratRt13(String text) {
         char[] nouText = new char[text.length()]; 
         for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) != ' ' || Character.isLetter(text.charAt(i))) {
-                nouText[i] = text.charAt(i);
-            }
-        }
-
-        for (int k = 0; k < nouText.length; k++) {
-            for (int j = 0; j < maj.length; j++) {
-                for (int i = 0; i < min.length; i++) {
-
+            if (Character.isUpperCase(text.charAt(i))) { 
+                for (int j = 0; j < maj.length; j++) {
+                    if (text.charAt(i) == maj[j]) {
+                        nouText[i] = maj[(j + 13) % maj.length];
+                        break;
+                    }
                 }
+            } else if (Character.isLowerCase(text.charAt(i))) {
+                for (int j = 0; j < min.length; j++) {
+                    if (text.charAt(i) == min[j]) {
+                        nouText[i] = min[(j + 13) % min.length];
+                        break;
+                    }
+                }
+            } else {
+                nouText[i] = text.charAt(i); 
             }
         }
-        return text;
+        return new String(nouText);
     }
 
     public static String desxifratRot13(String text) {
@@ -26,6 +32,8 @@ public class Rot13 {
 
     public static void main(String[] args) {
         String text = "Hola que tal!";
-        xifratRt13(text);
+        String xifrat = xifratRt13(text);
+        System.out.println("Xifrat: " + xifrat);
+
     }   
 }
