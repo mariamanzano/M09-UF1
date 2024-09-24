9c77ed50948911becffd9b85c4395eb0a677e758
Maria <mmanzanoroldan@gmail.com>
2024-09-24 10:53:11 +0200

    Fet el dexifrat i implementat l'scanner

--- Activitat1/Rot13.java
+++ Activitat1/Rot13.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Rot13 {
     public static char[] maj = {'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     public static char[] min = {'a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
@@ -27,13 +29,37 @@ public class Rot13 {
     }
 
     public static String desxifratRot13(String text) {
-        return text;
+        char[] nouText = new char[text.length()]; 
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) { 
+                for (int j = 0; j < maj.length; j++) {
+                    if (text.charAt(i) == maj[j]) {
+                        nouText[i] = maj[(j - 13 + maj.length) % maj.length];
+                        break;
+                    }
+                }
+            } else if (Character.isLowerCase(text.charAt(i))) {
+                for (int j = 0; j < min.length; j++) {
+                    if (text.charAt(i) == min[j]) {
+                        nouText[i] = min[(j - 13 + min.length) % min.length];
+                        break;
+                    }
+                }
+            } else {
+                nouText[i] = text.charAt(i); 
+            }
+        }
+        return new String(nouText);
     }
 
     public static void main(String[] args) {
-        String text = "Hola que tal!";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escriu un text: ");
+        String text = scanner.nextLine();
         String xifrat = xifratRt13(text);
         System.out.println("Xifrat: " + xifrat);
-
+        String desxifrat = desxifratRot13(xifrat);
+        System.out.println("Desxifrat: " + desxifrat);
+        scanner.close();
     }   
 }
