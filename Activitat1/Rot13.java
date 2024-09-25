509617fe8e70f3bb4ca856a57f68720ec8d55a5a
Maria Manzano Roldán <2023_maria.manzano@iticbcn.cat>
2024-09-25 21:38:46 +0200

    Afegits els accents i corregit error de la vista dels caracter especials

--- Activitat1/Rot13.java
+++ Activitat1/Rot13.java
@@ -1,65 +1,61 @@
-import java.util.Scanner;
-
 public class Rot13 {
-    public static char[] maj = {'A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    public static char[] min = {'a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    public static char[] maj = {'A', 'À', 'Á', 'B', 'C', 'Ç', 'D', 'E', 'È', 'É', 'F', 'G', 'H', 'I', 'Ì', 'Í', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'Ò', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ù', 'Ú', 'V', 'W', 'X', 'Y', 'Z'};
+    public static char[] min = {'a', 'à', 'á', 'b', 'c', 'ç', 'd', 'e', 'è', 'é', 'f', 'g', 'h', 'i', 'ì', 'í', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'ò', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ù', 'ú', 'v', 'w', 'x', 'y', 'z'};
 
     public static String xifratRt13(String text) {
-        char[] nouText = new char[text.length()]; 
+        String codText = "";
         for (int i = 0; i < text.length(); i++) {
             if (Character.isUpperCase(text.charAt(i))) { 
                 for (int j = 0; j < maj.length; j++) {
-                    if (text.charAt(i) == maj[j]) {
-                        nouText[i] = maj[(j + 13) % maj.length];
+                    if (maj[j] == text.charAt(i)) {
+                        codText += maj[(j + 13) % maj.length];
                         break;
                     }
                 }
             } else if (Character.isLowerCase(text.charAt(i))) {
                 for (int j = 0; j < min.length; j++) {
-                    if (text.charAt(i) == min[j]) {
-                        nouText[i] = min[(j + 13) % min.length];
+                    if (min[j] == text.charAt(i)) {
+                        codText += min[(j + 13) % min.length];
                         break;
                     }
                 }
             } else {
-                nouText[i] = text.charAt(i); 
+                codText += text.charAt(i); 
             }
         }
-        return new String(nouText);
+        return codText;
     }
 
     public static String desxifratRot13(String text) {
-        char[] nouText = new char[text.length()]; 
+        String codText = "";
         for (int i = 0; i < text.length(); i++) {
             if (Character.isUpperCase(text.charAt(i))) { 
                 for (int j = 0; j < maj.length; j++) {
-                    if (text.charAt(i) == maj[j]) {
-                        nouText[i] = maj[(j - 13 + maj.length) % maj.length];
+                    if (maj[j] == text.charAt(i)) {
+                        codText += maj[(j - 13 + maj.length) % maj.length];
                         break;
                     }
                 }
             } else if (Character.isLowerCase(text.charAt(i))) {
                 for (int j = 0; j < min.length; j++) {
-                    if (text.charAt(i) == min[j]) {
-                        nouText[i] = min[(j - 13 + min.length) % min.length];
+                    if (min[j] == text.charAt(i)) {
+                        codText += min[(j - 13 + min.length) % min.length];
                         break;
                     }
                 }
             } else {
-                nouText[i] = text.charAt(i); 
+                codText += text.charAt(i); 
             }
         }
-        return new String(nouText);
+        return codText;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Escriu un text: ");
-        String text = scanner.nextLine();
+        String text = "La façana de l'edifici és molt bonica";
+        System.out.println("Text: " + text);
         String xifrat = xifratRt13(text);
         System.out.println("Xifrat: " + xifrat);
         String desxifrat = desxifratRot13(xifrat);
         System.out.println("Desxifrat: " + desxifrat);
-        scanner.close();
     }   
 }
