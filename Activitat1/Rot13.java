c2f27da2cd75a4f6d20aafc9bb5a6c96c35e21f8
Maria Manzano Roldán <2023_maria.manzano@iticbcn.cat>
2024-09-26 17:27:55 +0200

    No se escriure

--- Activitat1/Rot13.java
+++ Activitat1/Rot13.java
@@ -2,7 +2,7 @@ public class Rot13 {
     public static final char[] MAJ = {'A', 'À', 'Á', 'B', 'C', 'Ç', 'D', 'E', 'È', 'É', 'F', 'G', 'H', 'I', 'Ì', 'Í', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'Ò', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ù', 'Ú', 'V', 'W', 'X', 'Y', 'Z'};
     public static final char[] MIN = {'a', 'à', 'á', 'b', 'c', 'ç', 'd', 'e', 'è', 'é', 'f', 'g', 'h', 'i', 'ì', 'í', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'ò', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ù', 'ú', 'v', 'w', 'x', 'y', 'z'};
 
-    public static String xifratRt13(String text) {
+    public static String xifratRot13(String text) {
         String codText = "";
         for (int i = 0; i < text.length(); i++) {
             if (Character.isUpperCase(text.charAt(i))) { 
@@ -55,8 +55,8 @@ public class Rot13 {
         String text2 = "El niño pidió piña para el desayuno.";
         System.out.println("Primera frase: " + text1);
         System.out.println("Segona frase: " + text2);
-        String xifrat1 = xifratRt13(text1);
-        String xifrat2 = xifratRt13(text2);
+        String xifrat1 = xifratRot13(text1);
+        String xifrat2 = xifratRot13(text2);
         System.out.println("Primer text xifrat: " + xifrat1);
         System.out.println("Segon text xifrat: " + xifrat2);
         String desxifrat1 = desxifratRot13(xifrat1);
