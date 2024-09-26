7bc4a41e680b530ecc660e0f5387e8dd3c59b8fb
Maria Manzano Roldán <2023_maria.manzano@iticbcn.cat>
2024-09-26 19:00:46 +0200

    Fet els jocs de prova i el desxifrat i el xifrat

---
+++ Activitat2/RotX.java
@@ -0,0 +1,84 @@
+public class RotX {
+    public static final char[] MAJ = {'A', 'À', 'Á', 'B', 'C', 'Ç', 'D', 'E', 'È', 'É', 'F', 'G', 'H', 'I', 'Ì', 'Í', 'Ï', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'Ò', 'Ó', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ù', 'Ú', 'Ü', 'V', 'W', 'X', 'Y', 'Z'};
+    public static final char[] MIN = {'a', 'à', 'á', 'b', 'c', 'ç', 'd', 'e', 'è', 'é', 'f', 'g', 'h', 'i', 'ì', 'í', 'ï', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'ò', 'ó', 'p', 'q', 'r', 's', 't', 'u', 'ù', 'ú', 'ü', 'v', 'w', 'x', 'y', 'z'};
+
+    public static String xifratRotX(String text, int desp) {
+        String codText = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) { 
+                for (int j = 0; j < MAJ.length; j++) {
+                    if (MAJ[j] == text.charAt(i)) {
+                        codText += MAJ[(j + desp) % MAJ.length];
+                        break;
+                    }
+                }
+            } else if (Character.isLowerCase(text.charAt(i))) {
+                for (int j = 0; j < MIN.length; j++) {
+                    if (MIN[j] == text.charAt(i)) {
+                        codText += MIN[(j + desp) % MIN.length];
+                        break;
+                    }
+                }
+            } else {
+                codText += text.charAt(i); 
+            }
+        }
+        return codText;
+    }
+
+    public static String desxifratRotX(String text, int desp) {
+        String codText = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) { 
+                for (int j = 0; j < MAJ.length; j++) {
+                    if (MAJ[j] == text.charAt(i)) {
+                        codText += MAJ[(j - desp + MAJ.length) % MAJ.length];
+                        break;
+                    }
+                }
+            } else if (Character.isLowerCase(text.charAt(i))) {
+                for (int j = 0; j < MIN.length; j++) {
+                    if (MIN[j] == text.charAt(i)) {
+                        codText += MIN[(j - desp + MIN.length) % MIN.length];
+                        break;
+                    }
+                }
+            } else {
+                codText += text.charAt(i); 
+            }
+        }
+        return codText;
+    }
+
+    public String forçaBrutaRotX(String text) {
+        for (int i = 0; i < MIN.length; i++) {
+            
+        }
+        return text;
+    }
+
+    public static void main(String[] args) {
+        String text1 = "La façana de l'edifici és molt bonica";
+        String text2 = "El niño pidió piña para el desayuno.";
+        int desp1 = 15;
+        int desp2 = 3;
+        System.out.println("Primera frase: " + text1);
+        System.out.println("Segona frase: " + text2);
+        String xifrat1 = xifratRotX(text1, desp1);
+        String xifrat2 = xifratRotX(text1, desp2);
+        String xifrat3 = xifratRotX(text2, desp1);
+        String xifrat4 = xifratRotX(text2, desp2);
+        System.out.println("Primer text xifrat amb un desplaçament de " + desp1 + ": " + xifrat1);
+        System.out.println("Primer text xifrat amb un desplaçament de " + desp2 + ": " + xifrat2);
+        System.out.println("Segon text xifrat amb un desplaçament de " + desp1 + ": " + xifrat3);
+        System.out.println("Segon text xifrat amb un desplaçament de " + desp2 + ": " + xifrat4);
+        String desxifrat1 = desxifratRotX(xifrat1, desp1);
+        String desxifrat2 = desxifratRotX(xifrat2, desp2);
+        String desxifrat3 = desxifratRotX(xifrat3, desp1);
+        String desxifrat4 = desxifratRotX(xifrat4, desp2);
+        System.out.println("Primer text desxifrat  amb un desplaçament de " + desp1 + ": " + desxifrat1);
+        System.out.println("Segon text desxifrat amb un desplaçament de " + desp2 + ": " +  desxifrat2);
+        System.out.println("Primer text desxifrat amb un desplaçament de " + desp1 + ": " +  desxifrat3);
+        System.out.println("Segon text desxifrat: amb un desplaçament de " + desp2 + ": " +  desxifrat4);
+    }   
+}
